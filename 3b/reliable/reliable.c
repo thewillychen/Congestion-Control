f820e04b399a49928835b4c20413c9557571c14e
Timesh Patel <timeshp@gmail.com>
2015-04-07 21:57:49 -0400

    made rwnd long

--- 3b/reliable/reliable.c
+++ 3b/reliable/reliable.c
@@ -296,7 +296,7 @@ void send_prepare(packet_t * packet){
   packet->ackno = htonl(packet->ackno);
   packet->seqno = htonl(packet->seqno);
   packet->len = htons(packet->len);
-  packet->rwnd = htons(packet->rwnd);
+  packet->rwnd = htonl(packet->rwnd);
   packet->cksum = cksum(packet, length);
 }
 
@@ -304,7 +304,7 @@ void read_prepare(packet_t * packet){
   packet->ackno = ntohl(packet->ackno);
   packet->seqno = ntohl(packet->seqno);
   packet->len = ntohs(packet->len);
-  packet->rwnd = ntohs(packet->rwnd);
+  packet->rwnd = ntohl(packet->rwnd);
 
 }
 
